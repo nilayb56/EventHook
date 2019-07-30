8d36ac40e699bb4e906f6ca04c091d400d2b5421
Nilay Bhatt <nilayb56@gmail.com>
2019-07-30 17:19:22 +0530

    Admin Index Activity deleted and Created Admin activity with Navigation Drawer

--- app/src/main/java/nilay/android/eventhook/AdminActivity.java
+++ app/src/main/java/nilay/android/eventhook/AdminActivity.java
@@ -18,35 +18,40 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.Menu;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 public class AdminActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    RelativeLayout rlUserRole,rlAddClg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+        DrawerLayout drawer = findViewById(R.id.drawer_admin);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
-
-        rlAddClg = (RelativeLayout) findViewById(R.id.rlAddClg);
-        rlUserRole = (RelativeLayout) findViewById(R.id.rlUserRole);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_admin);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -57,7 +62,7 @@ public class AdminActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.admin, menu);
+        //getMenuInflater().inflate(R.menu.admin, menu);
         return true;
     }
 
@@ -67,7 +72,7 @@ public class AdminActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        Toast.makeText(AdminActivity.this, String.valueOf(id), Toast.LENGTH_SHORT).show();
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -79,22 +84,31 @@ public class AdminActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        Fragment fragment = null;
+        Class fragmentClass = null;
         int id = item.getItemId();
-        Toast.makeText(AdminActivity.this, String.valueOf(id), Toast.LENGTH_SHORT).show();
-        if (id == R.id.adminAddClg) {
-            Toast.makeText(AdminActivity.this, "Add College", Toast.LENGTH_SHORT).show();
-            rlAddClg.setVisibility(View.VISIBLE);
-            rlUserRole.setVisibility(View.GONE);
-        } else if (id == R.id.adminAddRole) {
-            Toast.makeText(AdminActivity.this, "Add User Role", Toast.LENGTH_SHORT).show();
-            rlAddClg.setVisibility(View.GONE);
-            rlUserRole.setVisibility(View.VISIBLE);
-        } else if (id == R.id.adminUpdtRole) {
-            Toast.makeText(AdminActivity.this, "Update User Role", Toast.LENGTH_SHORT).show();
+
+        if (id == R.id.nav_adminAddClg) {
+            fragmentClass = AddCollegeFragment.class;
+        } else if (id == R.id.nav_adminAddRole) {
+
+        } else if (id == R.id.nav_adminUpdtRole) {
+
+        } else if (id == R.id.nav_adminReport) {
+
         }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flAdmin, fragment).commit();
+        item.setChecked(true);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_admin);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
